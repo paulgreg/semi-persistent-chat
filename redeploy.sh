1a1eb359e43c53833600fc1aa2b8ce7ff4cade92
Grégory Paul <paulgreg@gmail.com>
2020-05-31 18:19:54 +0200

    add npm i

--- redeploy.sh
+++ redeploy.sh
@@ -1,1 +1,1 @@
-sudo git pull --rebase && sudo ./build.sh && sudo chown -R www-data: . && pm2 status && pm2 restart semi-persistent-chat
+sudo git pull --rebase && npm i && sudo ./build.sh && sudo chown -R www-data: . && pm2 status && pm2 restart semi-persistent-chat
