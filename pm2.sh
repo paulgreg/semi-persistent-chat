62c55aaaa8d87b1ddcf614749e6bb291bc3cc2c7
Grégory Paul <me@gpaul.me>
2024-12-26 19:10:16 +0100

    chore: add NODE_ENV to pm2 script

--- pm2.sh
+++ pm2.sh
@@ -1,1 +1,1 @@
-pm2 start src/server/index.mjs --name 'semi-persistent-chat' --max-memory-restart 128M
+NODE_ENV=production pm2 start src/server/index.mjs --name 'semi-persistent-chat' --max-memory-restart 128M
