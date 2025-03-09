b1a6966975051e3fadf6864a7dc95087824743e5
Grégory Paul <me@gpaul.me>
2025-03-09 23:05:07 +0100

    fix: update start command

--- pm2.sh
+++ pm2.sh
@@ -1,1 +1,1 @@
-NODE_ENV=production pm2 start src/server/index.mjs --name 'semi-persistent-chat' --max-memory-restart 128M
+NODE_ENV=production pm2 start src/server/index.ts --name 'semi-persistent-chat' --max-memory-restart 128M
