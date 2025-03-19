09fb7e34b182a03656d9a5e416f144ed9638fd5e
Grégory Paul <me@gpaul.me>
2025-03-19 08:38:00 +0100

    chore: transpile server code

--- pm2.sh
+++ pm2.sh
@@ -1,1 +1,1 @@
-NODE_ENV=production pm2 start src/server/index.ts --name 'semi-persistent-chat' --max-memory-restart 128M
+NODE_ENV=production pm2 start dist/server/index.js --name 'semi-persistent-chat' --max-memory-restart 128M
